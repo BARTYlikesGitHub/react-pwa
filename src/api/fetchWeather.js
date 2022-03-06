3ba5dbc1189ac59815d5cbd8bab4d85cf31015a5
BARTYlikesGitHub <100819562+BARTYlikesGitHub@users.noreply.github.com>
2022-03-06 13:22:29 +1030

    feat: added display to see weather from input city

--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const URL = 'https://api.openweathermap.org/data/2.5/weather';
-const API_KEY = 'f33a484cf794d08d0148764789aaba32';
+// Had to generate my own API key 
+const API_KEY = '64891ad2e52c5d38ef0ae335accfbb01';
 
 export const fetchWeather = async (query) => {
     const data = await axios.get(URL, {
