8397833f01994e56e7743062404f5b9c95ead2e7
BARTYlikesGitHub <100819562+BARTYlikesGitHub@users.noreply.github.com>
2022-03-06 14:18:45 +1030

    chore: fixes

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,38 +1,42 @@
-const CACHE_NAME = "version-1"
-const urlsToCache = ["index.hmtl", "offline.html"]
+const CACHE_NAME = "version-1";
+const urlsToCache = ['index.html', 'offline.html'];
 
-const self = this
+const self = this;
 
-// Install Service Worker
-self.addEventListener("install", (event) => {
+// Install SW
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log("Opened Cache");
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
   )
-})
+});
 
-// Listen for Requests
-self.addEventListener("fetch", (event) => {
+// Listen for requests
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then(() => {
-      return fetch(event.request).catch(() => caches.match("offline.html"));
-    })
+    caches.match(event.request)
+      .then(() => {
+        return fetch(event.request)
+          .catch(() => caches.match('offline.html'))
+      })
   )
-})
+});
 
-// Active Service Worker
-self.addEventListener("activate", (event) => {
+// Activate the SW
+self.addEventListener('activate', (event) => {
   const cacheWhitelist = [];
   cacheWhitelist.push(CACHE_NAME);
-  event.waitUntil(caches.keys().then((cacheNames) => Promise.all(
-    cacheNames.map((cacheNames) => {
-      if (!cacheWhitelist.includes(cacheNames)) {
-        return caches.delete(cacheNames);
-      }
-    })
+
+  event.waitUntil(
+    caches.keys().then((cacheNames) => Promise.all(
+      cacheNames.map((cacheName) => {
+        if (!cacheWhitelist.includes(cacheName)) {
+          return caches.delete(cacheName);
+        }
+      })
+    ))
   )
-  )
-  )
-})
+});
